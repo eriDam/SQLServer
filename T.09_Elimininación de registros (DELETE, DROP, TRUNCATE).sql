47b69d26eb0258e47c9ed38e4e3c97633be77090
EriDam <erika.c.caballero@gmail.com>
2016-07-31 14:31:01 +0200

    Segundo commit: Uso de Truncate, DROP, Delete

--- T.09_Elimininación de registros (DELETE, DROP, TRUNCATE).sql
+++ T.09_Elimininación de registros (DELETE, DROP, TRUNCATE).sql
@@ -6,26 +6,34 @@ DROP --> elimina la estructura dela tabla
 
 TRUNCATE --> elimina los registros (reset)
 */
+--Creo una Tabla temporal de forma gráfica con algunos registros
 
+--Creo unos registros de forma 
 insert into Table_1 values('karla',21);
 insert into Table_1 values('maria',19);
 insert into Table_1 values('nancy',23);
 
 SELECT * FROM Table_1
 
+--Eliminamos todas las filas de la Tabla
 Delete  FROM Table_1 
 --WHERE id_usuario >= 3
  
-
+--Borra sólo 1 registro
 SELECT  * FROM usuarios
 Delete  FROM usuarios 
 WHERE id_usuario = 7
 
+--Creo unos registros 
 insert into Table_1 values('karla',21);
 insert into Table_1 values('maria',19);
 insert into Table_1 values('nancy',23);
 
+--Elimina absolutamente todo, resetea la tabla, si hay un campo autoincrementable
+--para que empiece la numeración correcta y no se queden los hueco que hemos borrado. 
 truncate table Table_1
+
+drop table Table_1
 
  
  
