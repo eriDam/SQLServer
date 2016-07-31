85796c1d97cd770673c304657286b73efc4a5ac5
EriDam <erika.c.caballero@gmail.com>
2016-07-31 14:35:58 +0200

    Tercer commit: T.10 Actualizar Registros

---
+++ T.10_Actualizar Registros .sql
@@ -0,0 +1,20 @@
+/*
+Tutoriales SQL Server#10 | Actualizar Registros 
+ UPDATE --> Actualizar registros
+ SET --> Asigna
+*/
+
+--Actualizaría todos los registros nonbre y pondrá carlos
+update usuarios set nombre = 'Carlos'
+
+-- 
+update usuarios set nombre = 'Carlos'
+where id_usuario = 2
+
+SELECT * FROM usuarios
+
+
+
+ 
+ 
+
