fead48b3e04c8dfd714aec595f6d201c3849f2cf
eridam <erika.c.caballero@gmail.com>
2017-10-22 13:18:49 +0200

    Cambios en scripts

--- T.05_Inserción de datos (INSERT).sql
+++ T.05_Inserción de datos (INSERT).sql
@@ -3,13 +3,13 @@ Server #5 | Inserción de datos (INSERT)
  
 */
 
---DROP TABLE usuarios
--- CREATE TABLE usuarios
--- (
--- id_usuario int primary Key,
--- nombre varchar(50) not null,
--- edad int not null
--- )
+--DROP TABLE usuarios2
+ CREATE TABLE usuarios
+ (
+ id_usuario int primary Key,
+ nombre varchar(50) not null,
+ edad int not null
+ )
 
 --Podemos insertar editando las  primeras filas o mediante INSERTS
 
@@ -18,3 +18,5 @@ insert into usuarios values(4,'karla',21);
 insert into usuarios values(5,'maria',19);
 insert into usuarios values(6,'nancy',23);
 
+
+select * from usuarios
--- T.07_WHERE.sql
+++ T.07_WHERE.sql
@@ -1,11 +1,19 @@
 /*
 Tutoriales SQL Server #07 | WHERE
+Es una plabara reservada que nos sirve para condicionar cierta operación de consulta
+Muetsra todos los datos mientras lo que condicione se cumpla.
+
  
 */
+--Añado un usuario mas llamado davis, para que hayan dos con el mismo nombre
+insert into usuarios values(7,'david',33)
 
- SELECT * FROM usuarios
+--2 registros cumplen la condición
+SELECT * FROM usuarios
+WHERE nombre= 'david'
 
- SELECT id_usuario,nombre FROM usuarios
+
+SELECT id_usuario,nombre FROM usuarios
 
 
  
