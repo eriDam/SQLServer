4f8be657d10c71177b81ec41e757a70d0fe9605c
eridam <erika.c.caballero@gmail.com>
2017-10-22 21:29:50 +0200

    new file:   T.50_Vistas_modificadas.sql         new file:   T.51_Lenguaje_de_control_de_flujo_CASE.sql

---
+++ T.50_Vistas_modificadas.sql
@@ -0,0 +1,28 @@
+/*
+Tutoriales SQL SERVER |  #50 Vistas modificadas
+
+Alter View
+*/
+
+/*
+*/
+
+-- Creacion de la vista
+create view copy 
+ with encryption as select * from usuarios 
+
+--Comprobamos la vista creada
+select * from copy
+
+
+--Comprobamos que este cifrada
+	sp_helptext copy
+
+--Emininacion de Vista
+	drop View copy 
+
+	
+--Alteramos la vista
+alter view copy
+as select * from usuarios where edad < 20
+ 
---
+++ T.51_Lenguaje_de_control_de_flujo_CASE.sql
@@ -0,0 +1,30 @@
+/*
+Tutoriales SQL SERVER |  #51  Lenguaje de Control de flujo [CASE]
+
+CASE
+
+
+*/
+
+
+/*Estructura de CASE*/
+--case 
+	--when   then 
+-- end
+--mostrar contenido
+
+select * from usuarios -- Muestra todos los datos
+
+select id_usuario,nombre,edad = -- Campos a mostrar
+case edad  --Campo a evaluar
+	when 17 then 'Menor' --Casos - ejecuta
+	when 19 then 'Mayor 19'
+	when 20 then 'Mayor 20'
+	when 21 then 'Mayor 21'
+	when 22 then 'Mayor 22'
+	when 23 then 'Mayor 23'
+	when 24 then 'Mayor 24'
+	when 25 then 'Mayor 25'
+end
+--Por defecto retorna un null, si no cumple la condición
+from usuarios --Tabla de donde vienen  los datos
